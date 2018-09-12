e7e3306ae28c5397f2be1715668c984aa86800fc
cygp <41449855+cygp@users.noreply.github.com>
2018-09-12 09:49:33 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@
           this.element = generateTemplate('board-template', { name: this.name, id: this.id });
           //create column
           this.element.querySelector('.board').addEventListener('click', function (event) {
-            if (event.target.classList.contains('btn-delete')) {
+            if (event.target.classList.contains('btn-delete-board')) {
               self.removeBoard();
             }
             if (event.target.classList.contains('create-column')) {
